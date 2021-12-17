5024d00c3daae71b6cda287310e74f5aae13638d
Nguyễn Văn Dũng <>
2021-12-17 21:31:35 +0700

    Fix menu

--- dist/widgets/Menu/icons/index.d.ts
+++ dist/widgets/Menu/icons/index.d.ts
@@ -17,3 +17,5 @@ export { default as TelegramIcon } from "./Telegram";
 export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
+export { default as RocketIcon } from "./Rocket";
+export { default as LotteryIcon } from "./Lottery";
